b386695a9f33d84aaf1af0ca91ccf1532df60142
BEYZA <BEYZA@DESKTOP-VFP0UBS>
2018-11-07 16:45:02 +0300

    addit

---
+++ MYARCH.CORE/MYARCH.UTILITIES/PassOperations/PassManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MYARCH.UTILITIES.StringOperations
+{
+    public static class PassManager
+    {
+        public static string Base64Encrypt(string data)
+        {
+            try
+            {
+                byte[] dataByteArray = System.Text.ASCIIEncoding.ASCII.GetBytes(data);
+                string encryptedData = System.Convert.ToBase64String(dataByteArray);
+                return encryptedData;
+            }
+            catch (Exception)
+            {
+                return data
+            }
+        }
+
+        public static string Base64Decrypt(string decryptData)
+        {
+            try
+            {
+                byte[] decryptDataArray = System.Convert.FromBase64String(decryptData);
+                string originalData = System.Text.ASCIIEncoding.ASCII.GetString(decryptDataArray);
+                return originalData;
+            }
+            catch (Exception)
+            {
+                return decryptData;
+            }
+        }
+    }
+}
---
+++ MYARCH.CORE/MYARCH.UTILITIES/StringOperations/StringManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MYARCH.UTILITIES.StringOperations
+{
+    public static class StringManager
+    {
+        public static string ToSlug(string incomingText)
+        {
+            incomingText = incomingText.Replace("ş", "s");
+            incomingText = incomingText.Replace("Ş", "s");
+            incomingText = incomingText.Replace("İ", "i");
+            incomingText = incomingText.Replace("I", "i");
+            incomingText = incomingText.Replace("ı", "i");
+            incomingText = incomingText.Replace("Ö", "o");
+            incomingText = incomingText.Replace("ö", "o");
+            incomingText = incomingText.Replace("ü", "u");
+            incomingText = incomingText.Replace("Ü", "u");
+            incomingText = incomingText.Replace("ç", "c");
+            incomingText = incomingText.Replace("Ç", "c");
+            incomingText = incomingText.Replace("ğ", "g");
+            incomingText = incomingText.Replace("Ğ", "g");
+            incomingText = incomingText.Replace(" ", "");
+            incomingText = incomingText.Replace("---", "-");
+            incomingText = incomingText.Replace("?", "");
+            incomingText = incomingText.Replace("/", "");
+            incomingText = incomingText.Replace(".", "");
+            incomingText = incomingText.Replace("'", "");
+            incomingText = incomingText.Replace("#", "");
+            incomingText = incomingText.Replace("", "");
+
+            string encodeUrl = (incomingText ?? "").ToLower();
+
+            encodeUrl = Regex.Replace(encodeUrl, @"[^a-z0-9]", "-");
+            encodeUrl = Regex.Replace(encodeUrl, @"-+", "-");
+            encodeUrl = Regex.Replace(encodeUrl, @"\&+", "and");
+
+            encodeUrl = encodeUrl.Replace("'", "");
+            encodeUrl.Trim('-');
+
+            return encodeUrl;
+        }
+    }
+}
